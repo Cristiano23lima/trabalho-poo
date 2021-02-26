bc8a37531c461e557948a5c8fa7f30d255e18d3c
Cristiano <dj7cristiano@gmail.com>
2021-02-25 23:56:35 -0300

    feat: add service cliente

--- src/main/java/com/ufc/poo/sorveteria/services/ClienteService.java
+++ src/main/java/com/ufc/poo/sorveteria/services/ClienteService.java
@@ -6,14 +6,51 @@
 package com.ufc.poo.sorveteria.services;
 
 import com.ufc.poo.sorveteria.model.Cliente;
+import com.ufc.poo.sorveteria.repository.ClienteRepository;
+import exceptions.NotFoundException;
 
 /**
  *
  * @author cristiano
  */
 public class ClienteService {
-    public Cliente cadastrar(Cliente cliente){
-        return null;
+    final private ClienteRepository clienteRepository;
+    
+    public ClienteService(ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+    
+    public Cliente cadastrar(Cliente cliente) throws NotFoundException{
+        try{
+            clienteRepository.save(cliente);
+            System.out.println("Cliente salvo com sucesso.\n");
+            return cliente;
+        }catch(NotFoundException e){
+            throw new NotFoundException(e.getMessage());
+        }
+    }
+    
+    public void remover(Integer id) throws NotFoundException{
+        try{
+             this.clienteRepository.remove(id);
+             System.out.println("Cliente removido com sucesso.\n");
+        }catch(NotFoundException e){
+            throw new NotFoundException(e.getMessage());
+        }
+    }
+    
+    public Cliente editar(Cliente cliente) throws NotFoundException{
+        try{
+            this.clienteRepository.edit(cliente);
+            System.out.println("Cliente editado com sucesso.\n");
+            return cliente;
+        }catch(NotFoundException e){
+            throw new NotFoundException(e.getMessage());
+        }
+    }
+    
+    public Cliente buscar(Integer id){
+        return this.clienteRepository.findById(id);
     }
         
 }
