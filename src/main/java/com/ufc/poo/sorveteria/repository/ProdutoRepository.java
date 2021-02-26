b90dbc02a3544cf6f53103c61fe5e62dffb5deda
Cristiano <dj7cristiano@gmail.com>
2021-02-25 23:44:48 -0300

    feat: repository produto

---
+++ src/main/java/com/ufc/poo/sorveteria/repository/ProdutoRepository.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ufc.poo.sorveteria.repository;
+
+import com.ufc.poo.sorveteria.model.Cliente;
+import com.ufc.poo.sorveteria.model.Produto;
+import exceptions.NotFoundException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author cristiano
+ */
+public class ProdutoRepository {
+    private List<Produto> produtos;
+    
+    public ProdutoRepository(){}
+    
+    public void save(Produto produto) throws NotFoundException{
+         if(produto == null){
+             throw new NotFoundException("Produto é nulo");
+         }
+         
+         produto.setCreatedAt(new Timestamp(new Date().getTime()));
+         
+         produtos.add(produto);
+    }
+    
+    public Produto findById(Integer id){
+        return produtos.stream().filter(produto -> produto.getId().equals(id)).findFirst().get();
+    }
+    
+    public void edit(Produto produto) throws NotFoundException{
+        Produto produtoEdit = this.findById(produto.getId());
+        if(produtoEdit == null){
+            throw new NotFoundException("Produto não encontrado.");
+        }
+        
+        if(!produto.getNome().isEmpty()){
+            produtoEdit.setNome(produto.getNome());
+        }
+        
+        if (produto.getTipo() != null) {
+            produtoEdit.setTipo(produto.getTipo());
+        }
+        
+        if (!produto.getPreco().isNaN()) {
+            produtoEdit.setPreco(produto.getPreco());
+        }
+        
+        produtoEdit.setUpdatedAt(new Timestamp(new Date().getTime()));
+        this.remove(produtoEdit.getId());//vai remover o valor antigo do array
+        
+        produtos.add(produtoEdit);//atualiza o array
+    }
+    
+    public void remove(Integer id) throws NotFoundException {
+        if(this.findById(id) == null){
+            throw new NotFoundException("Cliente não encontrado");
+        }
+        
+        produtos = produtos.stream().filter(produtoSalvo -> !produtoSalvo.getId().equals(id)).collect(Collectors.toList());
+    }
+}
