de1b089238b9ba39420cfa3013a4bab904dd4e30
Matheus Paz <matheuspazmpl@gmail.com>
2021-03-02 21:07:31 -0300

    Revert "fix: verificando se o cliente e valido"
    This reverts commit 6c60e11218b7e19f5248cb7b10947de1b9ec4e31.

--- src/main/java/com/ufc/poo/sorveteria/services/impl/ClienteServiceImpl.java
+++ src/main/java/com/ufc/poo/sorveteria/services/impl/ClienteServiceImpl.java
@@ -27,13 +27,9 @@ public class ClienteServiceImpl implements ClienteService{
     @Override
     public Cliente cadastrar(Cliente cliente) throws NotFoundException, BadAttributeValueExpException{        
         try{
-            if(cliente.verificarCliente()){//só salva se o cliente for válido
-                clienteRepository.save(cliente);
-                System.out.println("Cliente salvo com sucesso.\n");
-                return cliente;
-            }else{
-                throw new BadAttributeValueExpException("Cliente possui campos inválidos.");
-            }
+            clienteRepository.save(cliente);
+            System.out.println("Cliente salvo com sucesso.\n");
+            return cliente;
         }catch (NotFoundException e) {
             throw new NotFoundException(e.getMessage());
         } catch ( BadAttributeValueExpException e){
