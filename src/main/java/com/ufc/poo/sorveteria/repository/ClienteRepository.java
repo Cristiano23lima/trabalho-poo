6d5bc579a000d8991eac16533a9278b1c0323905
Cristiano <dj7cristiano@gmail.com>
2021-03-25 00:26:37 -0300

    fix: criando as querys do banco

--- src/main/java/com/ufc/poo/sorveteria/repository/ClienteRepository.java
+++ src/main/java/com/ufc/poo/sorveteria/repository/ClienteRepository.java
@@ -5,8 +5,12 @@
  */
 package com.ufc.poo.sorveteria.repository;
 
+import com.ufc.poo.sorveteria.config.ConexaoMysql;
 import com.ufc.poo.sorveteria.model.Cliente;
 import com.ufc.poo.sorveteria.exceptions.NotFoundException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -39,8 +43,9 @@ public class ClienteRepository {
         cliente.verificarCliente();//caso o cliente esteja ok ele segue adiante, se não ele joga uma excessão
 
         cliente.setCreatedAt(new Timestamp(new Date().getTime()));
-
-        clientes.add(cliente);
+        
+        //INSTRUÇÃO SQL
+        this.insertSql(cliente);
     }
 
     
@@ -50,7 +55,7 @@ public class ClienteRepository {
         }
 
         try{
-            return clientes.stream().filter(cliente -> cliente.getId().equals(id)).findFirst().get();
+            return this.whereSql(id);
         }catch(Exception e){
             return null;//caso não existe nenhum valor, ele irá retornar um erro, por isso o return null aqui
         }
@@ -70,9 +75,8 @@ public class ClienteRepository {
         clienteEdit.setTelefone(cliente.getTelefone());
         
         clienteEdit.setUpdatedAt(new Timestamp(new Date().getTime()));
-        this.remove(clienteEdit.getId());// vai remover o valor antigo do array
-
-        clientes.add(clienteEdit);// atualiza o array
+        
+        this.updateSql(clienteEdit);
     }
 
     
@@ -81,8 +85,131 @@ public class ClienteRepository {
             throw new NotFoundException("Cliente não encontrado");
         }
 
-        clientes = clientes.stream().filter(clienteSalvo -> !clienteSalvo.getId().equals(id))
-                .collect(Collectors.toList());
+        this.deleteSql(id);
     }
+    
+    private void deleteSql(Integer id){
+        String sql = "DELETE FROM cliente WHERE id = ?";
+        
+        Connection conexao = null;
+        PreparedStatement ps = null;
+        
+        try{
+            conexao = ConexaoMysql.openConnection();
+            ps = conexao.prepareStatement(sql);
+            
+            ps.setInt(1, id);
+            
+            ps.execute();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(ps != null) ps.close();
+                if(conexao != null) conexao.close();
+            }catch(Exception e){ e.printStackTrace();}
+        }
+    }
+    
+    private void insertSql(Cliente cliente){
+        String sql = "INSERT INTO cliente(nome, cpf, telefone, createdAt, updatedAt)"+
+                " VALUES(?,?,?,?,?)";
+        Connection conexao = null;
+        PreparedStatement ps = null;
+        
+        try{
+            conexao = ConexaoMysql.openConnection();
+            ps = conexao.prepareStatement(sql);
+            ps.setString(1, cliente.getNome());
+            ps.setString(2, cliente.getCpf());
+            ps.setString(3, cliente.getTelefone());
+            ps.setTimestamp(4, cliente.getCreatedAt());            
+            ps.setTimestamp(5, cliente.getUpdatedAt());
 
+            ps.execute();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+           
+                if(ps != null){
+                    ps.close();
+                }
+                
+                if(conexao != null){
+                    conexao.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private void updateSql(Cliente cliente){
+        String sql = "UPDATE cliente SET nome = ?, cpf = ?, telefone = ?, updatedAt = ? WHERE id = ?";
+        Connection conexao = null;
+        PreparedStatement ps = null;
+        
+        try{
+            conexao = ConexaoMysql.openConnection();
+            ps = conexao.prepareStatement(sql);
+            ps.setString(1, cliente.getNome());
+            ps.setString(2, cliente.getCpf());
+            ps.setString(3, cliente.getTelefone());       
+            ps.setTimestamp(4, cliente.getUpdatedAt());
+            ps.setInt(5, cliente.getId());
+
+            ps.execute();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+           
+                if(ps != null){
+                    ps.close();
+                }
+                
+                if(conexao != null){
+                    conexao.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private Cliente whereSql(Integer id){
+        String sql = "SELECT * FROM cliente WHERE id = ?";
+        
+        Connection conexao = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+        Cliente cliente = new Cliente();
+        
+        try{
+            conexao = ConexaoMysql.openConnection();
+            ps = conexao.prepareStatement(sql);
+            
+            ps.setInt(1, id);
+            
+            resultSet = ps.executeQuery();
+            
+            while(resultSet.next()){
+                cliente.setCpf(resultSet.getString("cpf"));
+                cliente.setNome(resultSet.getString("nome"));
+                cliente.setTelefone(resultSet.getString("telefone"));
+                cliente.setCreatedAt(resultSet.getTimestamp("createdAt"));
+                cliente.setUpdatedAt(resultSet.getTimestamp("updatedAt"));
+                cliente.setId(resultSet.getInt("id"));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(ps != null) ps.close();
+                if(conexao != null) conexao.close();
+            }catch(Exception e){ e.printStackTrace();}
+        }
+        return cliente;
+    }
 }
